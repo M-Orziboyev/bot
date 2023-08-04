04847e1324aafffdf298f04ca6d19bba07c090bc
M-Orziboyev <orziboyevmuzaffar@gmail.com>
2023-08-04 20:05:49 +0500

    Node telegram bot api && Bot commands

---
+++ index.js
@@ -0,0 +1,32 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = '6472379583:AAEmhU6jEb5bEEmG1JYymny6V2wIy7qi6lE'
+
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on('message', async (msg) => {
+    const chatId = msg.chat.id;
+    const text = msg.text
+
+
+    bot.setMyCommands([
+        {
+            command: '/start',
+            description: 'Bot haqida qisqacha ma\'\lumot'
+        },
+        {
+            command: '/info',
+            description: 'Sizning telegram profilingiz haqida ma\'\lumotlar'
+        }
+    ])
+
+    if (text === '/start') {
+        return bot.sendMessage(chatId, `Assalamu alaykum, ${msg.from?.first_name} Uzbeklog botiga xush kelibsiz!`)
+    }
+    if (text === '/info') {
+        return bot.sendMessage(chatId, `Birinchi ismingiz (first_name) - ${msg.from?.first_name} \n Ikkinchi ismingiz (last_name) - ${msg.from?.last_name} \n Sizning IDingiz - ${msg.from?.id} \n Telegram til versiyangiz - ${msg.from?.language_code}`)
+    }
+    bot.sendMessage(chatId, 'Men sizni tushinmadim):')
+}
+
+);
