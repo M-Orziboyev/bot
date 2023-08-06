6a407455d55861d05a16608b257124dbc4cee0da
M-Orziboyev <orziboyevmuzaffar@gmail.com>
2023-08-06 23:59:36 +0500

    update

--- index.js
+++ index.js
@@ -4,11 +4,7 @@ const token = '6472379583:AAEmhU6jEb5bEEmG1JYymny6V2wIy7qi6lE'
 
 const bot = new TelegramBot(token, { polling: true });
 
-bot.on('message', async (msg) => {
-    const chatId = msg.chat.id;
-    const text = msg.text
-
-
+const boostrap = () => {
     bot.setMyCommands([
         {
             command: '/start',
@@ -20,13 +16,23 @@ bot.on('message', async (msg) => {
         }
     ])
 
-    if (text === '/start') {
-        return bot.sendMessage(chatId, `Assalamu alaykum, ${msg.from?.first_name} Uzbeklog botiga xush kelibsiz!`)
-    }
-    if (text === '/info') {
-        return bot.sendMessage(chatId, `Birinchi ismingiz (first_name) - ${msg.from?.first_name} \n Ikkinchi ismingiz (last_name) - ${msg.from?.last_name} \n Sizning IDingiz - ${msg.from?.id} \n Telegram til versiyangiz - ${msg.from?.language_code}`)
-    }
-    bot.sendMessage(chatId, 'Men sizni tushinmadim):')
-}
+    bot.on('message', async (msg) => {
+        const chatId = msg.chat.id;
+        const text = msg.text
 
-);
+
+
+        if (text === '/start') {
+            return bot.sendMessage(chatId, `Assalamu alaykum, ${msg.from?.first_name} Uzbeklog botiga xush kelibsiz!`)
+            
+        }
+        if (text === '/info') {
+            return bot.sendMessage(chatId, `Birinchi ismingiz (first_name) - ${msg.from?.first_name} \n Ikkinchi ismingiz (last_name) - ${msg.from?.last_name} \n Sizning IDingiz - ${msg.from?.id} \n Telegram til versiyangiz - ${msg.from?.language_code}`)
+        }
+        bot.sendMessage(chatId, 'Men sizni tushinmadim):')
+    }
+
+    );
+
+}
+boostrap()
